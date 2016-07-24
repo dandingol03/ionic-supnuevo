d4863d79751b915a256275cc057c0e67b1b6ba3d
王俊杰 <danding@wangjunjiedeMacBook-Pro.local>
2016-07-24 18:21:59 +0800

    zyy

--- www/supnuevo/js/thefifth.js
+++ www/supnuevo/js/thefifth.js
@@ -1,52 +1,78 @@
 angular.module('app')
   .controller('theFifthController',function($scope,$state,$ionicLoading,$http,$cordovaProgress,$ionicModal) {
-    $scope.catalogArrFirsts="";
-    $scope.catalogArrSeconds="";
-    $scope.catalogArrThirds="";
-    $scope.catalogFirst="";
+    $scope.catalogArrFirsts=new Array;
+    $scope.catalogArrSeconds=new Array;
+    $scope.catalogArrThirds=new Array;
+    $scope.catalogFirst={};
     $scope.catalogSecond="";
     $scope.catalogThird="";
-    $scope.taxArrFirsts="";
-    $scope.taxArrSeconds="";
-    $scope.taxFirst="";
-    $scope.taxSecond="";
-    $scope.sizeUnitArrs="";
+    $scope.sizeValueArray =new Array;
+    $scope.sizeValue="";
+    $scope.sizeUnitArray=new Array;
+    $scope.sizeUnit="";
+    $scope.taxArrs=new Array;
+    $scope.tax="";
 
 
-    $http.get("/proxy/supnuevo/supnuevoSupnuevoCommonCommodityModifyInitMobile.do?parentId=''")
-      .success(function(response) {
-        if (response.catalogArr !== undefined || response.catalogArr !== null || response.catalogArr !== "") {
-          var catalogArr = response.catalogArr;
-          for (var i = 0; i < catalogArr.length; i++) {
-            var o = {value: '', label: ''};
-            o.label = catalogArr[i].label;
-            o.value = catalogArr[i].value;
-            $scope.catalogArrFirsts.push(o);
-          }
-        };
-        if(response.taxArr!==undefined||response.taxArr!==null||response.taxArr!==""){
-          var taxArr=response.taxArr;
-          for (var i=0;i<taxArr.length;i++){
-            var o={value:'',label:''};
-            o.label=taxArr[i].label;
-            o.value=taxArr[i].value;
-            $scope.taxArrFirsts.push(o);
-          }
+    $scope.selected={};
 
-        };
-        if(response.sizeUnitArr!==undefined||response.sizeUnitArr!==null||response.sizeUnitArr!==""){
-          var sizeUnitArr=response.sizeUnitArr;
-          for(i=0;i<sizeUnitArr.length;i++){
-           var o={value:'',label:''};
-            o.label=sizeUnitArr[i].label;
-            o.value=sizeUnitArr[i].value;
-            $scope.sizeUnitArrs.push(o);
-          }
-        }
-      })
-      .error(function(){
-        alert("error");
-      });
+
+    $scope.model = [{
+      id: 10001,
+      mainCategory: '男',
+      productName: '水洗T恤',
+      productColor: '白'
+    }, {
+      id: 10002,
+      mainCategory: '女',
+      productName: '圆领短袖',
+      productColor: '黑'
+    }, {
+      id: 10003,
+      mainCategory: '女',
+      productName: '短袖短袖',
+      productColor: '黃'
+    }];
+
+    $scope.getSelected=function(){
+      console.log('....'+$scope.selected.product);
+      console.log('..');
+    }
+
+    //$http.get("/proxy/supnuevo/supnuevoSupnuevoCommonCommodityModifyInitMobile.do?parentId=''")
+    //  .success(function(response) {
+    //    if (response.catalogArr !== undefined || response.catalogArr !== null || response.catalogArr !== "") {
+    //      var catalogArr = response.catalogArr;
+    //      for (var i = 0; i < catalogArr.length; i++) {
+    //        var o =new Object();
+    //        o.label = catalogArr[i].label;
+    //        o.value = catalogArr[i].value;
+    //        $scope.catalogArrFirsts.push(o);
+    //      }
+    //    };
+    //    if(response.taxArr!==undefined||response.taxArr!==null||response.taxArr!==""){
+    //      var taxArr=response.taxArr;
+    //      for (var i=0;i<taxArr.length;i++){
+    //        var o={value:'',label:''};
+    //        o.label=taxArr[i].label;
+    //        o.value=taxArr[i].value;
+    //        $scope.taxArrs.push(o);
+    //      }
+    //
+    //    };
+    //    if(response.sizeUnitArr!==undefined||response.sizeUnitArr!==null||response.sizeUnitArr!==""){
+    //      var sizeUnitArr=response.sizeUnitArr;
+    //      for(i=0;i<sizeUnitArr.length;i++){
+    //       var o={value:'',label:''};
+    //        o.label=sizeUnitArr[i].label;
+    //        o.value=sizeUnitArr[i].value;
+    //        $scope.sizeUnitArrs.push(o);
+    //      }
+    //    }
+    //  })
+    //  .error(function(){
+    //    alert("error");
+    //  });
     $scope.barCodes = new Array();
     $scope.selectedCode = {codeNum:''};
     $scope.goods={};
@@ -71,8 +97,16 @@ angular.module('app')
         $scope.barcodeModal.show();
       }
     }
+
+
+    $scope.selectChange=function(){
+      var fi=$scope.catalogFirst;
+      console.log('fi=' + fi);
+    }
+
     $scope.getcatalogSecondInfo=function()
     {
+      console.log('id of ob='+ob.id);
       $http({
         method:"post",
         params:{
@@ -83,8 +117,6 @@ angular.module('app')
 
         },
       }).success(function(response){
-
-
         if(response.errorMessage !== null && response.errorMessage !== undefined && response.errorMessage !== ""){
           $cordovaProgress.hide();
           alert(response.errorMessage);
@@ -112,6 +144,9 @@ angular.module('app')
         });
       })
     }
+
+
+
     $scope.gecatalogThirdInfo=function()
     {
       $http({
